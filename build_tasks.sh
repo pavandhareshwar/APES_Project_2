13597972e53b31a833cb9f57547409c14ea1767f
Pavan Dhareshwar <pavan.dhareshwar@colorado.edu>
2018-04-08 22:54:53 -0600

    Added a line to build the decision task in build script

--- build_tasks.sh
+++ build_tasks.sh
@@ -2,11 +2,12 @@
 
 rm Main_Task/main_task
 rm Logger_Task/logger_task
+rm Decision_Task/decision_task
 
 gcc Logger_Task/logger_task.c -o Logger_Task/logger_task -lrt -lpthread
 
 #gcc socket_task.c -o socket_task -lrt -lpthread
 
-#gcc decision_task.c -o decision_task -lrt -lpthread
+gcc Decision_Task/decision_task.c -o Decision_Task/decision_task -lrt -lpthread
 
 gcc Main_Task/main_task.c -o Main_Task/main_task -lrt -lpthread
