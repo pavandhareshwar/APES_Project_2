1be91df3405cf58f7bf64a3a54612edbce54f44f
Pavan Dhareshwar <pavan.dhareshwar@colorado.edu>
2018-04-09 00:05:34 -0600

    Added a line to build the socket task in build script

--- build_tasks.sh
+++ build_tasks.sh
@@ -3,11 +3,12 @@
 rm Main_Task/main_task
 rm Logger_Task/logger_task
 rm Decision_Task/decision_task
+rm Socket_Task/socket_task
+
+gcc Main_Task/main_task.c -o Main_Task/main_task -lrt -lpthread
 
 gcc Logger_Task/logger_task.c -o Logger_Task/logger_task -lrt -lpthread
 
-#gcc socket_task.c -o socket_task -lrt -lpthread
+gcc Socket_Task/socket_task.c -o Socket_Task/socket_task -lrt -lpthread
 
 gcc Decision_Task/decision_task.c -o Decision_Task/decision_task -lrt -lpthread
-
-gcc Main_Task/main_task.c -o Main_Task/main_task -lrt -lpthread
