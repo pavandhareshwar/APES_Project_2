799b880465970fc85d51ad758687db2692600f29
pavandhareshwar <pavan.dhareshwar@colorado.edu>
2018-04-19 18:34:23 -0600

    Updated the build script file to reflect the renaming of Socket task to Comm task

--- BBG_Tasks/build_tasks.sh
+++ BBG_Tasks/build_tasks.sh
@@ -3,14 +3,14 @@
 rm Main_Task/main_task
 rm Logger_Task/logger_task
 rm Decision_Task/decision_task
-rm Socket_Task/socket_task
+rm Comm_Task/comm_task
 rm UART_Task/uart_task
 
 gcc Main_Task/main_task.c -o Main_Task/main_task -lrt -lpthread
 
 gcc Logger_Task/logger_task.c -o Logger_Task/logger_task -lrt -lpthread
 
-gcc Socket_Task/socket_task.c -o Socket_Task/socket_task -lrt -lpthread
+gcc Comm_Task/comm_task.c -o Comm_Task/comm_task -lrt -lpthread
 
 gcc Decision_Task/decision_task.c -o Decision_Task/decision_task -lrt -lpthread
 
